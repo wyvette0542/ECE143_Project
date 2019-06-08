2cd7d64d1f176c099c31e5c2fafc3bbaaff5eca2
justinstaana <38512958+justinstaana@users.noreply.github.com>
2019-06-07 22:43:04 -0700

    Update create_heatmap_overall.py

--- create_heatmap_overall.py
+++ create_heatmap_overall.py
@@ -7,6 +7,8 @@ import numpy as np
 
 '''
 This file is used to create a heatmap that compares the highest paying jobs vs. the lowest paying jobs per state
+Input: Pandas Dataframe
+Output: Plotly Heat Map
 '''
 
 plotly.tools.set_credentials_file(username='wyvette0542', api_key='9IqIPiGXjRvppiBBZSER')   # Credentials to use plotly
