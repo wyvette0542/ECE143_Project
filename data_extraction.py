aaec8cb49573ff86e0c8b62c512bde17033048c6
sel118 <sel118@ucsd.edu>
2019-06-07 22:19:40 -0700

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,7 +1,6 @@
 import numpy as np
 import re
 import pandas as pd
-import matplotlib.pyplot as plt 
 
 # Obtain all Languages to be analyzed
 with open('languages.txt', 'r')as f:
@@ -114,21 +113,14 @@ left = range(1,51)
 height = language_counts
 tick_label = languages
   
-# plotting a bar chart for languages vs number of jobs with programming language as a requirement
-plt.bar(left, height, tick_label = tick_label,
-        width = 0.5, color = ['red', 'green']) 
-plt.xticks(left, tick_label, rotation='vertical')
-plt.tick_params(axis='x', which='major', labelsize=7)
-
-# naming the x-axis 
-plt.xlabel('Programming Languages')
-# naming the y-axis 
-plt.ylabel('Number of jobs') 
-# plot title 
-plt.title('Programming Languages vs Number of Jobs with Programming Language as a Requirement') 
-  
-# function to show the plot 
-plt.show() 
+# create a new txt file containing the counts of programming languages
+f = open('language_counts.txt','w')
+i = 0
+for val in language_counts:
+    line = str(read_languages[i][:-1]) + ': ' + str(val) + '\n'
+    f.write(line)
+    i = i + 1
+f.close()
 
 # Location Analysis
 possible_locations = []
@@ -141,6 +133,15 @@ for possible_location in possible_locations:
     amount = locations.count(possible_location)
     location_counts.append(amount)
 
+# create a new txt file containing the counts of locations
+f = open('location_counts.txt','w')
+i = 0
+for val in location_counts:
+    line = str(possible_locations[i]) + ': ' + str(val) + '\n'
+    f.write(line)
+    i = i + 1
+f.close()
+
 # State Analysis
 possible_states = []
 for i in range(0, len(states)):
@@ -151,6 +152,15 @@ state_counts = []
 for possible_state in possible_states:
     amount = states.count(possible_state)
     state_counts.append(amount)
+
+# create a new txt file containing the counts of states
+f = open('state_counts.txt','w')
+i = 0
+for val in state_counts:
+    line = str(possible_states[i]) + ': ' + str(val) + '\n'
+    f.write(line)
+    i = i + 1
+f.close()
 
 # Store data into First Dataframe
 loc_sal_lang['Location'] = locations
@@ -179,124 +189,7 @@ language_data['Salary'] = salaries_per_language
 language_data['Location'] = locations_per_language
 language_data['States'] = states_per_language
 
-# Location vs Salary
-grp = loc_sal_lang.groupby('Location')
-
-f = open('location_salary.txt','w')
-for key, item in grp:
-    line = 'Location: ' + str(key) + '; Average Salary: ' + str(item['Salary'].mean()) + '\n'
-    f.write(line)
-
-f.close()
-
-# Language vs Salary, Language vs Location
-grp2 = language_data.groupby('Language')
-f = open('language_salary.txt','w')
-g = open('language_location_counts.txt', 'w')
-all_languages = []
-avg_salaries_languages = []
-for key, item in grp2:
-    line = 'Language: ' + str(key) + '; Average Salary: ' + str(item['Salary'].mean()) + '\n'
-    if (str(key) == 'arduino') or (str(key) == 'c') or (str(key) == 'cplusplus') or (str(key) == 'c3') or (str(key) == 'erlang'):
-        all_languages.append(str(key))
-        avg_salaries_languages.append(item['Salary'].mean())
-    
-    elif (str(key) == 'python') or (str(key) == 'java') or (str(key) == 'javascript') or (str(key) == 'qml') or (str(key) == 'sql'):
-        all_languages.append(str(key))
-        avg_salaries_languages.append(item['Salary'].mean())
-        
-    elif (str(key) == 'css') or (str(key) == 'r') or (str(key) == 'csharp') or (str(key) == 'matlab') or (str(key) == 'go'):
-        all_languages.append(str(key))
-        avg_salaries_languages.append(item['Salary'].mean())
-        
-    f.write(line)
-    
-    values = item['Location'].value_counts()
-    vals = str(values.astype(str))
-    top_loc = vals.splitlines()[0]
-    if key == 'rust':
-        top_loc = vals.splitlines()[1]
-    top_loc = top_loc.split()
-    top_loc = top_loc[0]
-    line2 = 'Language: ' + str(key) + '; Locations: ' + top_loc + '\n'
-    g.write(line2)
-f.close()
-g.close()
-
 # Save Data Frames
 loc_sal_lang.to_pickle("./dataframe1.pkl")
 
 language_data.to_pickle("./dataframe2.pkl")
-
-# Group by States
-grp3 = language_data.groupby('States')
-
-f = open('states_salary.txt','w')
-all_states = []
-avg_salaries_state = []
-for key, item in grp3:
-    line = 'State: ' + str(key) + '; Average Salary: ' + str(item['Salary'].mean()) + '\n'
-    f.write(line)
-    all_states.append(str(key).upper())
-    avg_salaries_state.append(item['Salary'].mean())
-f.close()
-
-# Salary vs State
-state_salary_dict = dict(zip(all_states, avg_salaries_state))
-sorted_salaries = (sorted(state_salary_dict.values(), reverse = True))
-sorted_states = []
-for i in range(len(sorted_salaries)):
-    for key in state_salary_dict:
-        if (sorted_salaries[i] == state_salary_dict[key]):
-            sorted_states.append(key)
-
-# Graph Parameters
-left = range(1,21)
-height = sorted_salaries
-tick_label = sorted_states
-  
-# plotting a bar chart for Average Salaries for Each State
-plt.bar(left, height, tick_label = tick_label,
-        width = 0.5, color = ['red']) 
-plt.xticks(left, tick_label, rotation=60)
-plt.tick_params(axis='x', which='major', labelsize=9)
-
-# naming the x-axis 
-plt.xlabel('States')
-# naming the y-axis 
-plt.ylabel('Avg Salary of Job') 
-# plot title 
-plt.title('Average Salaries of Jobs for Each State') 
-  
-# function to show the plot 
-plt.show() 
-
-# Salary vs Language
-language_salary_dict = dict(zip(all_languages, avg_salaries_languages))
-sorted_salaries_lang = (sorted(language_salary_dict.values(), reverse = True))
-sorted_langs = []
-for i in range(len(sorted_salaries_lang)):
-    for key in language_salary_dict:
-        if (sorted_salaries_lang[i] == language_salary_dict[key]):
-            sorted_langs.append(key)
-
-# Graph Parameters
-left = range(1,16)
-height = sorted_salaries_lang
-tick_label = sorted_langs
-  
-# plotting a bar chart for Avarage Salaries of Jobs for Each Language
-plt.bar(left, height, tick_label = tick_label,
-        width = 0.5, color = ['red']) 
-plt.xticks(left, tick_label, rotation=60)
-plt.tick_params(axis='x', which='major', labelsize=9)
-
-# naming the x-axis 
-plt.xlabel('Language')
-# naming the y-axis 
-plt.ylabel('Avg Salary of Job') 
-# plot title 
-plt.title('Average Salaries of Jobs for Each Language') 
-  
-# function to show the plot 
-plt.show() 
